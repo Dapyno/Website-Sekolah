b0ed21d2d32f5666a2b6a4dc850807a53ff8b56f
decka <jaketbochil@gmail.com>
2026-08-28 12:37:49 +0700

    ambudeck

--- Admin/guru/create.php
+++ Admin/guru/create.php
@@ -111,7 +111,7 @@ if($_SERVER['REQUEST_METHOD'] == 'POST') {
         }
     </script>
     
-    <!-- Bootstrap Icons -->
+    <!- Bootstrap Icons ->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.css" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700;800&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="../css/guru/create.css">
